3da5cf68f15297c8192d51d36c81484599228a96
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-26 21:21:34 -0300

    criacao do mudule de usuarios

---
+++ src/usuario/usuario.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+import { UsuarioController } from './usuario.controller';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
+@Module({
+  providers: [
+    UsuarioService,
+    PrismaService
+  ],
+  controllers: [UsuarioController]
+})
+export class UsuarioModule { }
