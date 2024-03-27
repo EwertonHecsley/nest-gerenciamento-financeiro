9043c4c9e39106d0fd4e2b46f9a8ee3436b4540b
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-26 22:03:22 -0300

    endpoint criar usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { UsuarioService } from './usuario.service';
+import { UsuarioDto } from './dto/usuario.dto';
+import { Response } from 'express';
 
 @Controller('usuario')
-export class UsuarioController {}
+export class UsuarioController {
+    constructor(
+        private readonly usuarioService: UsuarioService
+    ) { }
+
+    @Post()
+    async create(@Body() dataUser: UsuarioDto, @Res() res: Response) {
+        const resultado = await this.usuarioService.create(dataUser);
+        const { senha: _, ...usuario } = resultado;
+
+        return res.status(HttpStatus.CREATED).json({ mensagem: 'Usuário cadastrado com sucesso.', usuario });
+    }
+}
