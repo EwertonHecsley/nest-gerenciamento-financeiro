ed1989c2304e6697c364498ab7e938c70ea5a6f4
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-26 21:57:52 -0300

    instalando brcypt e servico para criar novo Usuario

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { UsuarioDto } from './dto/usuario.dto';
 
 @Injectable()
 export class UsuarioService {
     constructor(
         private readonly prisma: PrismaService
     ) { }
+
+    async create(dataUser: UsuarioDto): Promise<UsuarioDto> {
+        const { nome, email, senha } = dataUser;
+
+        const buscaEmail = await this.prisma.usuario.findUnique({ where: { email } });
+        if (buscaEmail) throw new HttpException('E-mail já utilizado em outro usuário.', HttpStatus.BAD_REQUEST);
+
+        const hashSenha = await bcrypt.hash(senha, 10);
+
+        return await this.prisma.usuario.create({
+            data: {
+                nome,
+                email,
+                senha: hashSenha
+            }
+        })
+    }
 }
